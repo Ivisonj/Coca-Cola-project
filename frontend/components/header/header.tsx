e29f6f61740d5707bb1b4735c3cffc948525a9af
ivison <ivison_j@outlook.com>
2023-05-30 23:44:55 -0300

    feat: first commit

---
+++ frontend/components/header/header.tsx
@@ -0,0 +1,17 @@
+'use client'
+import React from "react"
+import { Container, Row } from './styles'
+
+export default function NavBar() {
+    return (
+        <>
+            <Container>
+                <Row></Row>
+                <Row>
+                    <img width="30%" height="100%" src="https://logosmarcas.net/wp-content/uploads/2020/08/Coca-Cola-Logo-1987-2009.png" alt="coca-cola" />
+                </Row>
+                <Row> </Row>
+            </Container>
+        </>
+    )
+}
---
+++ frontend/components/header/styles.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const Container = styled.div `
+    width: 100%;
+    height: 60px;
+    background-color: #fff;
+    // box-shadow: 0px  2px 4px rgba(0, 0, 0, 0.4);
+    display: flex;
+    flex-direction: row;
+`
+
+export const Row = styled.div `
+    width: 33%;
+    height: 100%;
+    display: flex;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
